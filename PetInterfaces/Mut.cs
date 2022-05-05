13d02519c105f3922a803e2395627db25e33dfc4
ABaldwin1297 <abaldwin1297@gmail.com>
2022-05-05 15:39:46 -0400

    Add project files.

---
+++ PetInterfaces/Mut.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetInterfaces {
+    internal class Mut : IDog {
+        public string Color { get; set; } = "Orange";
+        public bool IsLarge { get; set; } = true;
+        public string Name { get; set; } = "Beans";
+        public string Bark() {
+            return ("Urf!"); 
+        }
+    }
+}
---
+++ PetInterfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using PetInterfaces;
+
+var Fred = new Boxer();
+var Roxy = new Bully();
+var Beans = new Mut();
+
+IDog[] dogs = {Fred, Roxy, Beans};
+
+foreach (var dog in dogs) {
+    Console.WriteLine($"Name {dog.Name}, color is {dog.Color}, and is {(dog.IsLarge ? "large" : "small")}" + 
+           $" and makes the sound {dog.Bark()}");
+}
